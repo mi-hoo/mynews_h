0a68d580ce3f17236cd0b66eadc79f563229f313
Miho Asai <mihoo519@gmail.com>
2020-03-10 00:53:55 +0000

    text18_kadai2

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 /*
 課題３
-Route::get('XXX','Admin\AAAController@bbb');
+Route::get('XXX','AAAController@bbb');
 */
