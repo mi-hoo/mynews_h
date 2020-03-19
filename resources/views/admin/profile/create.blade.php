44bc32675410ee71d34e959f12c577d466d9f870
Miho Asai <mihoo519@gmail.com>
2020-03-19 14:16:12 +0000

    text22_kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,6 +11,39 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>MYプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create')}}"method="post">
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{old('name')}}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                        <div class="form-check-inline">
+                         <input class="form-check-input" type="radio" name="gender">
+                         <label class="form-check-label" for="radio1">男性</label>
+                        </div>
+                        <div class="form-check-inline">
+                         <input class="form-check-input" type="radio" name="gender">
+                         <label class="form-check-label" for="radio2">女性</label>
+                        </div>
+                        </div>
+　　　　　　　　　　</div>
+                    <div class="form-group row">
+                         <label class="col-md-2">趣味</label>
+                         <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{old('hobby')}}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
