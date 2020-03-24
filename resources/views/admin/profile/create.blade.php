1e499dd9ea4de42805121885a95a0a3abd2b1b63
Miho Asai <mihoo519@gmail.com>
2020-03-24 04:10:14 +0000

    text23_kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -12,6 +12,14 @@
             <div class="col-md-8 mx-auto">
                 <h2>MYプロフィール</h2>
                 <form action="{{ action('Admin\ProfileController@create')}}"method="post">
+                    @csrf
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
                     <div class="form-group row">
                         <label class="col-md-2">氏名</label>
                         <div class="col-md-10">
@@ -22,11 +30,11 @@
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
                         <div class="form-check-inline">
-                         <input class="form-check-input" type="radio" name="gender">
+                         <input class="form-check-input" type="radio" name="gender" value="male" checked="{{old('gender') =="male" ? 'checked':''}}" >
                          <label class="form-check-label" for="radio1">男性</label>
                         </div>
                         <div class="form-check-inline">
-                         <input class="form-check-input" type="radio" name="gender">
+                         <input class="form-check-input" type="radio" name="gender" value="female" checked="{{old('gender') =="female" ? 'checked':''}}">
                          <label class="form-check-label" for="radio2">女性</label>
                         </div>
                         </div>
@@ -43,6 +51,7 @@
                             <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
+                    <input type="submit" class="btn btn-primary" value="更新">
                 </form>
             </div>
         </div>
